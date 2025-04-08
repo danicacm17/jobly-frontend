957737c7890ac0a25254a845ebc5e21ed8f3485a
Danica <danicacm@live.com>
2025-04-08 10:35:06 -0400

    Updated CSS and corrected missing job search bar

--- src/pages/JobList.jsx
+++ src/pages/JobList.jsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import JoblyApi from "../api";
 import JobCard from "../components/JobCard";
 
-/** JobList: displays all available jobs */
+/** Lists all jobs with optional search. */
 function JobList() {
   const [jobs, setJobs] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     async function fetchJobs() {
@@ -14,16 +15,45 @@ function JobList() {
     fetchJobs();
   }, []);
 
+  async function handleSearch(evt) {
+    evt.preventDefault();
+    const jobs = await JoblyApi.getJobs(searchTerm.trim());
+    setJobs(jobs);
+  }
+
   return (
     <div className="JobList">
       <h1>Jobs</h1>
-      {jobs.length ? (
-        jobs.map(job => <JobCard key={job.id} job={job} />)
-      ) : (
-        <p>Loading...</p>
-      )}
+
+      <form onSubmit={handleSearch}>
+        <input
+          name="searchTerm"
+          value={searchTerm}
+          placeholder="Search jobs..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+
+      <div className="JobList-list">
+        {jobs.length ? (
+          jobs.map((job) => (
+            <JobCard
+              key={job.id}
+              id={job.id}
+              title={job.title}
+              salary={job.salary}
+              equity={job.equity}
+              companyName={job.companyName}
+            />
+          ))
+        ) : (
+          <p>No results found.</p>
+        )}
+      </div>
     </div>
   );
 }
 
 export default JobList;
+
